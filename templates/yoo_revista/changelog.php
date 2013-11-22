374e5c4a52be19b95d1ca38043346da808ed5cc8
hieu <zunz700@yahoo.com.vn>
2013-11-22 16:13:06 +0700

    install template Yoo-Revista

---
+++ templates/yoo_revista/changelog.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @package   yoo_revista
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) YOOtheme GmbH
+* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+*/
+
+// no direct access
+die('Restricted access');
+
+?>
+
+Changelog
+------------
+
+1.0.4
+^ Updated menu icons and titles according to Warp 6.2
+^ Updated wrapper box-sizing according to Warp 6.2
+
+1.0.3
+# Fixed SVG backgrounds for Chrome browser
+
+1.0.2
+# Fixed Backgrounds for iPad
+
+1.0.1
+^ Switched mobile menubar to black
+
+1.0.0
++ Initial Release
+
+
+
+* -> Security Fix
+# -> Bug Fix
+$ -> Language fix or change
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
